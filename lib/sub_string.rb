f836eacea0894307c7eadeaf9e01d6e3ae4f16c5
Sam Russell <sam.russell@powershop.co.nz>
2015-10-12 16:25:13 +1300

    regexes are faster

--- lib/sub_string.rb
+++ lib/sub_string.rb
@@ -25,16 +25,6 @@ class SubString
     end
   end
 
-  # load full blocks instead, handle overlaps differently
-  #def load_next_block
-  #  amount_to_load = BLOCK_SIZE
-  #  overlap = @target_string.size - 1
-  #  amount_to_load -= overlap if @block
-  #  last_bit_of_block = @block ? @block[-overlap..-1] : ''
-  #  next_chunk_from_file = file.read(amount_to_load)
-  #  @block = next_chunk_from_file ? last_bit_of_block + next_chunk_from_file : nil
-  #end
-  
   def load_next_block
     if @block
       file.read(BLOCK_SIZE, @block)
@@ -51,7 +41,8 @@ class SubString
   end
 
   def handle_end_of_block
-    (1...@target_string.size).each do |offset|
+    @target_string_enumerator ||= (1...@target_string.size)
+    @target_string_enumerator.each do |offset|
       offset_in_block = @block.size - @target_string.size + offset
       end_of_block_match = smaller_parts(@target_string)[-offset][0]
       if @block.index(end_of_block_match, offset_in_block)
@@ -65,24 +56,16 @@ class SubString
     @file ||= File.open(@filename, 'r')
   end
 
+  def target_regex
+    @target_regex ||= Regexp.new(@target_string)
+  end
+
   # should be "count_occurrences...." or the like
   def find_target_string_in_block
-    #target_string_start_points_in_block = (0..block.size-@target_string.size)
     skip_until = 0
-    #target_string_start_points_in_block.each do |start_point|
-    #  next if start_point < skip_until
-    #  string_to_compare = block[start_point...start_point+@target_string.size]
-    #  if string_to_compare == @target_string
-    #    @occurrences_of_target_string += 1
-    #    skip_until = start_point + @target_string.size
-    #  end
-    #end
-    while index = @block.index(@target_string, skip_until)
+    while index = @block.index(target_regex, skip_until)
       @occurrences_of_target_string +=1
       skip_until = index + @target_string.size
     end
-  end
-
-  def string_matches?(base_string, offset, target_string)
   end
 end
